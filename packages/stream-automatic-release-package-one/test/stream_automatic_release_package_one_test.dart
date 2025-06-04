e9011903d2bb573bb0c6ee1f2d516667cb27f11b
Sahil Kumar <sahil@getstream.io>
2025-06-04 14:32:01 +0200

    Initial commit

---
+++ packages/stream-automatic-release-package-one/test/stream_automatic_release_package_one_test.dart
@@ -0,0 +1,16 @@
+import 'package:stream_automatic_release_package_one/stream_automatic_release_package_one.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
---
+++ packages/stream-automatic-release-package-two/test/stream_automatic_release_package_two_test.dart
@@ -0,0 +1,16 @@
+import 'package:stream_automatic_release_package_two/stream_automatic_release_package_two.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
